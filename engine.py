482195632f136a6407a4101133930f3365f617ab
larrabyte <larrabyte@protonmail.ch>
2021-03-13 18:18:15 +1100

    Add a method to find opposite directions
    Now the Room's link() method actually works :)
    Also imported annotations from __future__ to allow type hints to reference their own classes.

--- engine.py
+++ engine.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from enum import Enum
 
 class Direction(Enum):
@@ -6,6 +7,16 @@ class Direction(Enum):
     SOUTH = 2
     WEST = 3
 
+    def opposite(self) -> Direction:
+        if self == Direction.NORTH:
+            return Direction.SOUTH
+        elif self == Direction.EAST:
+            return Direction.WEST
+        elif self == Direction.SOUTH:
+            return Direction.NORTH
+
+        return Direction.EAST
+
 class Room:
     def __init__(self, name: str) -> None:
         self.name = name
@@ -13,10 +24,10 @@ class Room:
         self.drops = []
         self.links = {}
 
-    def link(self, direction: Direction, destination: object) -> None:
+    def link(self, direction: Direction, destination: Room) -> None:
         """Link two rooms together, adding entries to both objects."""
         self.links[direction] = destination
-        destination.links[direction - 2] = self
+        destination.links[direction.opposite()] = self
 
 class Character:
     def __init__(self, name: str, health: int) -> None:
